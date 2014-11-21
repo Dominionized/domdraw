b5c077223bd216c6ad397389baf2f1822a2b5eee
dominique09 <dominique.septembre@gmail.com>
2014-11-21 10:48:41 -0500

    Server branch added

--- src/ca/csf/domdraw/server/Server.java
+++ src/ca/csf/domdraw/server/Server.java
@@ -1,9 +1,21 @@
 package ca.csf.domdraw.server;
 
-/**
- * Created by dom on 21/11/14.
- */
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
+	
+	private static ServerSocket serverSocket;
+	private static Socket clientSocket;
+	private static ObjectInputStream input;
+	private static ObjectOutputStream output;
+	private static int port;
+	private static final int DEFAULT_PORT = 4444;
+	private static final int DEFAULT_L33T_LEVEL = 30;
+	
+	
     public void main(String[] args){
         System.out.println("Hello world");
     }
